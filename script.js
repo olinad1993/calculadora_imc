b2f2fe60868df0942489edd9e5ba72010ac34219
Danilo Ferreira <85970991+olinad1993@users.noreply.github.com>
2022-02-01 18:36:11 -0300

    Pagina para limpeza

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-function calcular(){
-    const altura = document.getElementById('txtaltura').value
-    const peso = document.getElementById('txtpeso').value
-    const result = document.getElementById('result')
-    
-    if(peso !== '' && altura !== ''){
-    }else {
-      alert('Preencha todos os campos')
-      return;
-    }
+function calcular() {
+  const altura = document.getElementById('txtaltura').value
+  const peso = document.getElementById('txtpeso').value
+  const result = document.getElementById('result')
 
-      const valorIMC = (peso/ (altura * altura)).toFixed(2);
-      let classif = '';
-  
-      if (valorIMC < 18.5){
-          classif = '<p>Você está abaixo do peso.</p>'
-      }else if (valorIMC < 25){
-          classif = '<p>Você está com peso ideal. Parabéns!!</p>'
-      }else if (valorIMC < 30){
-          classif = '<p>Você está levemente acima do peso.</p>'
-      }else if(valorIMC < 35) {
-          classif = '<p>Você está com obesidade GRAU I.</p>'
-      } else if (valorIMC < 40){
-        classif = '<p>Você está com obesidade GRAU II.</p>'
-      } else{
-        classif = '<p>Você está com obesidade GRAU III. Cuidado!!</p>'
-      }
-       
-      result.innerHTML = `<p>Seu IMC é <strong>${valorIMC}</strong>.</p> <p>${classif}</p>`;
+  if (peso !== '' && altura !== '') {} else {
+    alert('Preencha todos os campos')
+    return;
   }
 
-function limpar(){
-  if(document.getElementById('txtaltura').value!=""){
-    document.getElementById('txtaltura').value="";
-    document.getElementById('txtpeso').value="";
+  const valorIMC = (peso / (altura * altura)).toFixed(2);
+  let classif = '';
+
+  if (valorIMC < 18.5) {
+    classif = '<p>Você está abaixo do peso.</p>'
+  } else if (valorIMC < 25) {
+    classif = '<p>Você está com peso ideal. Parabéns!!</p>'
+  } else if (valorIMC < 30) {
+    classif = '<p>Você está levemente acima do peso.</p>'
+  } else if (valorIMC < 35) {
+    classif = '<p>Você está com obesidade GRAU I.</p>'
+  } else if (valorIMC < 40) {
+    classif = '<p>Você está com obesidade GRAU II.</p>'
+  } else {
+    classif = '<p>Você está com obesidade GRAU III. Cuidado!!</p>'
   }
+
+  result.innerHTML = `<p>Seu IMC é <strong>${valorIMC}</strong>.</p> <p>${classif}</p>`;
 }
